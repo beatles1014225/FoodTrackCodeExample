fcbbc10984700324316787bc938e8ebe80467183
beatles1014225 <beatles1014225@gmail.com>
2017-12-20 03:37:55 +0100

    FoodTracks

---
+++ RulesCalled.py
@@ -0,0 +1,102 @@
+import subprocess
+import os
+
+class RulesCalled(object,):
+    def __init__(self, pathTowrite):
+        self.pathTowrite = pathTowrite
+
+    def writeRule(self,command,nameFile):
+        lpFile = open(self.pathTowrite + nameFile, 'w')
+        subprocess.call("(cd D:\Diego\MASTER\Master Thesis\Clingon\clingo-5.2.1-win64 && " + command + ")", shell=True, stdout=lpFile)
+        lpFile.close()
+
+
+    def eachRule(self, ruleID,solution,*args):
+        if not solution:
+            ASPmainFile = open("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\usecase1V1.1Temp.lp",'r')
+        elif solution == "main":
+            ASPmainFile = open("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\solutions.lp",'r')
+        elif solution == "individual":
+            ASPmainFile = open("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\solutionsIndividual.lp",'r')
+        file = ASPmainFile.readlines()
+        TempText = ""
+        startIndex = 0
+        index = 0
+        ruleID = ruleID
+        if len(args) > 0 and os.stat("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\" + args[0]).st_size == 0:
+            print("Empty file")
+            return
+        else:
+            for i in file:
+                if i == '\n':
+                    TempText = self.writefileRule(startIndex,index,file,TempText,ruleID)
+                    if len(TempText) > 20:
+                        lpFile = open("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\TempRule.lp", 'w')
+                        lpFile.write(TempText)
+                        lpFile.close()
+                        if len(args) == 0:
+                            command = "clingo-python.exe Rules.lp helperRules.lp  TempRule.lp " \
+                                      "python_functions.lp linesTrenchTwenteMedium2.lp circleTwenteMedium2.lp otherUtilitiesTwenteMedium2.lp  " \
+                                      "parametersTrenchTwenteMedium2.lp soilCharacteristicsMedium2.lp polygonTwenteMedium2.lp linesVehicleSegmentMedium2.lp  " \
+                                      "parametersOtherPipesTwenteMedium2.lp polygonEquipment_areas.lp polygonPiled_soil_areas.lp RulesConstrain.lp  combinationsMainNetwork.lp " \
+                                      "combinationsIndividualNetwork.lp "
+                        else:
+                            command = "clingo-python.exe Rules.lp helperRules.lp  TempRule.lp " \
+                                      "python_functions.lp linesTrenchTwenteMedium2.lp circleTwenteMedium2.lp otherUtilitiesTwenteMedium2.lp  " \
+                                      "soilCharacteristicsMedium2.lp polygonTwenteMedium2.lp linesVehicleSegmentMedium2.lp  " \
+                                      "parametersOtherPipesTwenteMedium2.lp polygonEquipment_areas.lp polygonPiled_soil_areas.lp RulesConstrain.lp  " \
+                                      "combinationsMainNetwork.lp combinationsIndividualNetwork.lp  " + args[0]
+                        self.writeRule(command,"file" + str(ruleID) + ".txt")
+                        print("File rule output " + str(ruleID) + " written")
+                        ruleID += 1
+
+                    elif TempText != "False":
+                        if len(args) == 0:
+                            command = "clingo-python.exe Rules.lp helperRules.lp  " \
+                                      "python_functions.lp linesTrenchTwenteMedium2.lp circleTwenteMedium2.lp otherUtilitiesTwenteMedium2.lp  " \
+                                      "parametersTrenchTwenteMedium2.lp soilCharacteristicsMedium2.lp polygonTwenteMedium2.lp linesVehicleSegmentMedium2.lp " \
+                                      "parametersOtherPipesTwenteMedium2.lp polygonEquipment_areas.lp polygonPiled_soil_areas.lp RulesConstrain.lp  combinationsMainNetwork.lp  " \
+                                      "combinationsIndividualNetwork.lp  " + TempText
+                        else:
+                            command = "clingo-python.exe Rules.lp helperRules.lp  " \
+                                      "python_functions.lp linesTrenchTwenteMedium2.lp circleTwenteMedium2.lp otherUtilitiesTwenteMedium2.lp  " \
+                                       + args[0] + "soilCharacteristicsMedium2.lp polygonTwenteMedium2.lp linesVehicleSegmentMedium2.lp " \
+                                      "parametersOtherPipesTwenteMedium2.lp polygonEquipment_areas.lp polygonPiled_soil_areas.lp RulesConstrain.lp  combinationsMainNetwork.lp  " \
+                                                   "combinationsIndividualNetwork.lp  " + TempText
+                        self.writeRule(command,"file" + str(ruleID) + ".txt")
+                        print("File rule output " + str(ruleID) + " written" )
+                        ruleID += 1
+                    TempText = ""
+                    startIndex = index + 1
+
+                index += 1
+
+
+    def writefileRule(self,start,end,file,TempText,ruleID):
+        rulesArray = ["rule12","rule13","rule21","rule22","rule23","rule24","rule25","rule26","rule27","rule28","rule30","rule31",
+                      "rule32","rule33"]
+        for eachRule in rulesArray:
+            if eachRule in file[start]:
+                RuleCompesed = True
+                break
+            else:
+                RuleCompesed = False
+
+        if RuleCompesed == True:
+
+            arr = os.listdir("D:\\Diego\\MASTER\\Master Thesis\\Clingon\\clingo-5.2.1-win64\\")
+
+            ruleName = eachRule
+
+            for i in arr:
+                if i == ruleName + ".lp":
+                    return(i)
+            return("False")
+
+        else:
+            while start <= end:
+                TempText = TempText + file[start]
+                start += 1
+            return(TempText)
+
+
---
+++ lines.py
@@ -0,0 +1,206 @@
+import dxfgrabber
+import random
+class LinesFeatures(object):
+    def __init__(self,dxfPathFile, lineLpPathFile):
+        self.path = dxfPathFile
+        self.dxf = dxfgrabber.readfile(dxfPathFile)
+        self.outputEntities = [entity for entity in self.dxf.entities]
+        self.lineLpPath = lineLpPathFile
+
+    def linexyzCoordinatesStartVertex(self):
+        list_of_verticesStart = [entity.start for entity in self.outputEntities if entity.dxftype == 'LINE']
+        return(list_of_verticesStart)
+
+    def linexyzCoordinatesEndVertex(self):
+        list_of_verticesEnd = [entity.end for entity in self.outputEntities if entity.dxftype == 'LINE']
+        return(list_of_verticesEnd)
+
+    def layerList(self):
+        layers = []
+        all_lines = [entity for entity in self.dxf.entities if entity.dxftype == 'LINE']
+        for line in all_lines:
+            layers.append(line.layer.lower())
+        return(layers)
+
+    def layerListPolyline(self):
+        layers = []
+        all_lines = [entity for entity in self.dxf.entities if entity.dxftype == 'LWPOLYLINE']
+        for line in all_lines:
+            layers.append(line.layer.lower())
+        return(layers[1])
+
+    def PolylineToLine(self):
+        linexyCoordinatesVertex = []
+        list_of_polylines = [entity.points for entity in self.outputEntities if entity.dxftype == 'LWPOLYLINE']
+
+        for polylines in list_of_polylines:
+            for vertex in polylines:
+                linexyCoordinatesVertex.append((vertex + (random.uniform(29.000, 39.000),)))
+        return(linexyCoordinatesVertex)
+
+
+    def lineGeometryOrganization(self,linexyzCoordinatesStartVertex,linexyzCoordinatesEndVertex,lineValue):
+        self.lineID = lineValue
+        valueIDLayer = lineValue
+        listLayers = self.layerList()
+        listLayersread = []
+        lpLinesFile = open(self.lineLpPath, 'w')
+        referenceCoordinates = []
+        for i in range(len(linexyzCoordinatesStartVertex)):
+            if not "section" in listLayers[i]:
+                linesVertexStart = linexyzCoordinatesStartVertex[i]
+                linesVertexEnd = linexyzCoordinatesEndVertex[i]
+                if i == 0:
+                    heightStart = random.uniform(29.000, 30.000)
+                    heightEnd = random.uniform(29.000, 30.000)
+                else:
+                    startHeightAssessment = binary_search(referenceCoordinatesSorted,linesVertexEnd[1])
+                    endHeightAssessment = binary_search(referenceCoordinatesSorted,linesVertexStart[1])
+                    if startHeightAssessment == False:
+                        heightEnd = random.uniform(29.000, 30.000)
+                    elif type(startHeightAssessment) == float:
+                        heightEnd = startHeightAssessment
+
+                    if endHeightAssessment == False:
+                        heightStart = random.uniform(29.000, 30.000)
+                    elif type(endHeightAssessment) == float:
+                        heightStart = endHeightAssessment
+                StringValuesStart = "line(l" + str(self.lineID) + ", " + '"' + str(linesVertexStart[0]) + '"' + ", " + '"' + \
+                                    str(linesVertexStart[1]) + '"' + ", " + '"' + str(heightStart) + '"' + ", "
+
+                StringValuesEnd = '"' + str(linesVertexEnd[0]) + '"' + ", " + '"' + str(linesVertexEnd[1]) + '"' + ", " + \
+                                  '"' + str(heightEnd) + '"' + ").\n"
+                StringValue = StringValuesStart + StringValuesEnd
+                valueIDLayer = self.indexIDLines(listLayersread, listLayers[i], valueIDLayer)
+                declarativeValues = (listLayers[i] + "(" + listLayers[i] + str(valueIDLayer) + ").\n")
+                representationValues = ( "representation" + "(" + listLayers[i] + str(valueIDLayer) + "," + "l" + str(self.lineID) + ").\n")
+
+                lpLinesFile.write(StringValue)
+                lpLinesFile.write(declarativeValues)
+                lpLinesFile.write(representationValues)
+
+                self.lineID += 1
+                listLayersread.append(listLayers[i])
+                referenceCoordinates.append((linesVertexStart[1],heightStart))
+                referenceCoordinates.append((linesVertexEnd[1], heightEnd))
+                referenceCoordinatesSorted = sorted(referenceCoordinates,key=lambda x: x[0])
+
+            else:
+                break
+        lpLinesFile.close()
+        print("lines lp file created")
+
+    def lineGeometryOrganizationOtherPipes(self,linexyzCoordinatesStartVertex,linexyzCoordinatesEndVertex,lineValue):
+        self.lineID = lineValue
+        valueIDLayer = lineValue
+        listLayers = self.layerList()
+        listLayersread = []
+        lpLinesFile = open(self.lineLpPath, 'w')
+        referenceCoordinates = []
+        for i in range(len(linexyzCoordinatesStartVertex)):
+            if not "section" in listLayers[i]:
+                linesVertexStart = linexyzCoordinatesStartVertex[i]
+                linesVertexEnd = linexyzCoordinatesEndVertex[i]
+                if i == 0:
+                    heightStart = random.uniform(29.700, 30.700)
+                    heightEnd = random.uniform(29.700, 30.700)
+                else:
+                    startHeightAssessment = binary_search(referenceCoordinatesSorted,linesVertexEnd[1])
+                    endHeightAssessment = binary_search(referenceCoordinatesSorted,linesVertexStart[1])
+                    if startHeightAssessment == False:
+                        heightEnd = random.uniform(29.700, 30.700)
+                    elif type(startHeightAssessment) == float:
+                        heightEnd = startHeightAssessment
+
+                    if endHeightAssessment == False:
+                        heightStart = random.uniform(29.700, 30.700)
+                    elif type(endHeightAssessment) == float:
+                        heightStart = endHeightAssessment
+                StringValuesStart = "line(l" + str(self.lineID) + ", " + '"' + str(linesVertexStart[0]) + '"' + ", " + '"' + \
+                                    str(linesVertexStart[1]) + '"' + ", " + '"' + str(heightStart) + '"' + ", "
+
+                StringValuesEnd = '"' + str(linesVertexEnd[0]) + '"' + ", " + '"' + str(linesVertexEnd[1]) + '"' + ", " + \
+                                  '"' + str(heightEnd) + '"' + ").\n"
+                StringValue = StringValuesStart + StringValuesEnd
+                valueIDLayer = self.indexIDLines(listLayersread, listLayers[i], valueIDLayer)
+                declarativeValues = ("other_pipes" + "(" + listLayers[i] + str(valueIDLayer) + ").\n")
+                typeService = ("service_other_pipe" + "(" + listLayers[i] + "," + listLayers[i] + str(valueIDLayer)+").\n")
+                representationValues = ( "representation" + "(" + listLayers[i] + str(valueIDLayer) + "," + "l" + str(self.lineID) + ").\n")
+
+                lpLinesFile.write(StringValue)
+                lpLinesFile.write(declarativeValues)
+                lpLinesFile.write(typeService)
+                lpLinesFile.write(representationValues)
+
+                self.lineID += 1
+                listLayersread.append(listLayers[i])
+                referenceCoordinates.append((linesVertexStart[1],heightStart))
+                referenceCoordinates.append((linesVertexEnd[1], heightEnd))
+                referenceCoordinatesSorted = sorted(referenceCoordinates,key=lambda x: x[0])
+
+            else:
+                break
+        lpLinesFile.close()
+        print("lines lp file with other pipe services created")
+
+    def PolylineTolineGeometryOrganization(self,linexyzCoordinatesVertex,lineValue):
+        self.lineID = lineValue
+        listLayers = self.layerListPolyline()
+        listLayersread = []
+        lpLinesFile = open(self.lineLpPath, 'w')
+        for i in range(len(linexyzCoordinatesVertex)):
+            if not "section" in listLayers and i < (len((linexyzCoordinatesVertex)) - 1):
+                linesVertexStart = linexyzCoordinatesVertex[i]
+                linesVertexEnd = linexyzCoordinatesVertex[i + 1]
+                StringValuesStart = "line(l" + str(self.lineID) + ", " + '"' + str(linesVertexStart[0]) + '"' + ", " + '"' + \
+                                    str(linesVertexStart[1]) + '"' + ", " + '"' + str(linesVertexStart[2]) + '"' + ", "
+
+                StringValuesEnd = '"' + str(linesVertexEnd[0]) + '"' + ", " + '"' + str(linesVertexEnd[1]) + '"' + ", " + \
+                                  '"' + str(linesVertexEnd[2]) + '"' + ").\n"
+                StringValue = StringValuesStart + StringValuesEnd
+                declarativeValues = str(listLayers) + "(" + str(listLayers) + str(self.lineID) + ").\n"
+                representationValues = "representation" + "(" + str(listLayers) + str(self.lineID) + "," + "l" + str(self.lineID) + ").\n"
+
+                lpLinesFile.write(StringValue)
+                lpLinesFile.write(declarativeValues)
+                lpLinesFile.write(representationValues)
+
+                self.lineID += 1
+                listLayersread.append(listLayers)
+            else:
+                break
+        lpLinesFile.close()
+        print("Polylines lp file created")
+
+    def indexIDLines(self,listLayers,layer, valueIDLayer):
+        if layer in listLayers and len(listLayers) == 1:
+            valueIDLayer += 1
+        elif layer in listLayers and listLayers[(len(listLayers) - 1)] == layer:
+            valueIDLayer += 1
+        elif layer in listLayers and listLayers[(len(listLayers) - 1)] != layer:
+            valueIDLayer = len(listLayers) - valueIDLayer + 1
+        else:
+            valueIDLayer = 1
+        return (valueIDLayer)
+
+    def finalLineID(self):
+        return(self.lineID)
+
+def binary_search(array, target):
+    lower = 0
+    upper = len(array)
+    while lower < upper:
+        x = lower + (upper - lower) // 2
+        val = array[x][0]
+        if target == val:
+            return array[x][1]
+        elif target > val:
+            if lower == x:
+                break
+            lower = x
+        elif target < val:
+            upper = x
+    return False
+
+
+
